09e91f04a6afe02c3be612a85616b39979f7ec11
saulariasd <saul_ariasd@hotmail.com>
2020-12-09 10:18:07 -0500

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,38 @@
+//  ================================
+//              PORT
+//  ================================
+
+process.env.PORT = process.env.PORT || 3000;
+
+//  ================================
+//             ENTORNO
+//  ================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//  ================================
+//      Venciminento del token
+//  ================================
+// 60 segungos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = "96h";
+
+//  ================================
+//      SEED de autenticacion
+//  ================================
+process.env.SEED = process.eventNames.SEED || "este-es-el-seed-de-desarrollo";
+
+//  ================================
+//                 BD
+//  ================================
+
+let urlDB;
+if (process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/santa-isabel-turistico";
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
+process.env.URLDB = urlDB;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+//habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+// Configuracion global de rutas
+app.use(require("./routes/index"));
+
+mongoose.connect(
+    process.env.URLDB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log("Base de datos online");
+    }
+);
+
+app.listen(process.env.PORT, () =>
+    console.log("Escuchando puerto:", process.env.PORT)
+);
