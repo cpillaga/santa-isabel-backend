a328ea5655596ed400f4d74ca21effd6835e35fb
Christian Pillaga <christian_pillaga_s@hotmail.es>
2021-01-26 23:04:19 -0500

    add Cors 2

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors({ origin: '*' }));
+
+
 app.use(require('./sector'));
 app.use(require('./actividad'));
 app.use(require('./festividad'));
